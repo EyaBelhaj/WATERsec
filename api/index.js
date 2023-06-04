158b677bc5016644fabb8f8b91a07fdd02427848
Eya Belhaj <ebelhaj81@gmail.com>
2023-06-04 02:45:00 +0200

    mqtt

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mqtt = require('mqtt');
 const app = express();
 const port = 8000;
 const cors = require("cors");
@@ -7,6 +8,19 @@ const userRoute = require("./routes/userRoute");
 const sensorRoute = require("./routes/sensorRoute");
 const consumptionRoutes = require("./routes/consumptionRoutes");
 
+const mqttBroker = 'mqtt://192.168.23.129:1883';
+const mqttTopic = "mytopic"; 
+
+const mqttClient = mqtt.connect(mqttBroker);
+// Gérer l'événement de connexion au broker MQTT
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker');
+  // S'abonner au topic MQTT
+  mqttClient.subscribe(mqttTopic);
+});
+mqttClient.on('error', (error) => {
+  console.error('connection failed', error)
+})
 app.use(cors());
 app.use(express.json());
 app.use("/", userRoute);
